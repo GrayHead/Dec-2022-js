f63b45a8fae1a6c16ef1e21f06d28151bc0a3fae
alex <AlexandrPetrov1987>
2023-01-31 21:17:14 +0200

    Add lesson1

---
+++ lessons/lesson1/main.js
@@ -0,0 +1,164 @@
+// console.log(33);  //вивод інформації в консоль розробника
+
+// document.write('Hello'); //вивод інформації в вікно браузера
+
+// let strText = 'Hello';// type string  'hello' "hello" `hello`
+// console.log(strText);
+//
+// const PI = 3.14;
+// console.log(PI);
+//
+// strText = 'World';
+// console.log(strText);
+//
+// // PI = 22;
+//
+// const num = 22;
+// // num = 33
+
+// {
+//     let a = 1;
+//     var b =2;
+//     console.log(a);
+//     console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+
+// let str = 'Hello';
+// console.log(typeof str);
+//
+// let num = 10;
+// console.log(typeof num);
+//
+// let bool = true;
+// console.log(typeof bool);
+//
+// let str2 = '10';
+// console.log(typeof str2);
+
+// let str = 'Hello';
+// let str2 = "World";
+// //conacat string
+// // console.log(str + str2);
+//
+// let str3 = 'OP';
+//
+// console.log(str + 'qqq' + str2 + ' ' + str3);
+//
+// console.log(`${str} brave new ${str2} ${str3}`);
+
+// let num = 10;
+// console.log(num);
+//
+// let num1 = 4;
+// console.log(num1);
+
+// console.log(num + num1);
+// console.log(num - num1);
+// console.log(num * num1);
+// console.log(num / num1);
+// console.log(num % num1);
+
+// console.log(0.1 + 0.2);
+
+// let big = BigInt(123435465767);
+// console.log(big);
+
+// let a = true;
+// let b = false;
+// console.log(typeof a);
+// console.log(typeof b);
+//
+// // > < >= <= == != === !==
+//
+// let res = 5 > 7;
+// console.log(res);
+//
+// console.log(10 === 10);
+// console.log(10 !== 10);
+// console.log(10 !== '10');
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+// console.log('hello' === 'hello');
+// console.log('hell' < 'hello');
+
+// console.log(typeof (22 + ''));
+//
+// console.log(100 + 200 + 'hello');
+//
+// console.log('hello' + 100 + 200);
+//
+// console.log(typeof +'12');
+// console.log(+'12acascahsjcnjasnjcl');
+//
+// console.log(parseInt('12wertgthjj11'));
+// console.log(parseFloat('12.45'));
+
+// console.log(!!10);
+// console.log(!!-10);
+// console.log(!!'-10');
+// console.log(!![]);
+// console.log(!!{});
+//
+// console.log(!!0);
+// console.log(!!'');
+// console.log(!!NaN);
+// console.log(!!null);
+// console.log(!!undefined);
+// console.log(!!-0);
+
+// let user = {
+//     name: 'Vasya',
+//     age: 33
+// }
+// console.log(user);
+// //
+// let arr = [1, 2, 3, 4, user];
+// console.log(arr);
+//
+// console.log(typeof obj);
+// console.log(typeof arr);
+
+// let undef = undefined;
+// console.log(undef);
+// console.log(typeof undef);
+//
+// let nan = NaN;
+// console.log(nan);
+// console.log(typeof nan);
+//
+// let nul = null;
+// console.log(nul);
+// console.log(typeof nul);
+
+
+//undefined
+// let a;
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
