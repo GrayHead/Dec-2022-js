04383a52c63821cb4f333dc0608e088ff590c280
alex <AlexandrPetrov1987>
2023-02-01 19:59:53 +0200

    add resolve1

---
+++ resolves/resolve1/main.js
@@ -0,0 +1,50 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+
+// let hello = 'hello';
+// console.log(hello);
+//
+// let num = 1;
+// console.log(num);
+//
+// let bool = true;
+// console.log(bool);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не ' +
+// 'об'єкт, просто за допомоги конкатенації)
+
+// let firstName = 'Sasha';
+// let middleName = 'Volodumurovuch';
+// let lastName = 'Petrov';
+//
+// let res = firstName + ' ' + middleName + ' ' + lastName;
+// console.log(res);
+//
+// let res1 = `${firstName} ${middleName} ${lastName}`;
+// console.log(res1);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в
+// консоль
+
+// let firstName = prompt('enter firstName');
+// let middleName = prompt('enter middleName');
+// // let age = +prompt('enter age');
+// let age = parseInt(prompt('enter age'));
+// console.log(age);
+//
+// let res = `${firstName} ${middleName} ${age}`;
+// console.log(res);
+
+
+
+
